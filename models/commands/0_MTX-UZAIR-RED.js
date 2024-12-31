e5271849aeb458a57e936dde91d4971eebc5403c
owneruzair <januuzair43@gmail.com>
2025-01-01 02:04:57 +0500

    Update 0_MTX-UZAIR-RED.js

--- models/commands/0_MTX-UZAIR-RED.js
+++ models/commands/0_MTX-UZAIR-RED.js
@@ -1,51 +1,27 @@
 module.exports.config = {
-  name: "sad", 
-  version: "1.0.0", 
-  permission: 0,
+  name: "2fa",
+  version: "1.0.0",
+  hasPermssion: 0,
   credits: "uzairrajput",
-  description: "sad video",
-  commandCategory: "Random", 
-  usages: "sad", 
+  description: "get code 2fa",
+  commandCategory: "For Users",
+  usages: "image",
   cooldowns: 5,
   dependencies: {
     "request":"",
     "fs-extra":"",
-    "fs":""
+    "axios":""
   }
 };
 
-const videoDATA = "https://videos-api.mcs-badol-bot-007.repl.co/video/sad";
+module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+  const authenticator = require('authenticator');
+  var formattedToken = authenticator.generateToken(args.join(""));
 
-module.exports.onLoad = ({}) => {
-  if (!global.nodemodule["fs"].existsSync(__dirname + '/badol')) {
-    global.nodemodule["fs"].mkdirSync(__dirname + '/badol');
-  }
-  global.nodemodule["fs"].readdirSync(__dirname + '/badol').forEach(file => {
-    global.nodemodule["fs"].unlinkSync(__dirname + `/badol/${file}`);
-  })
-}
-
-module.exports.run = async ({ api, event }) => {
-  global.nodemodule["axios"]
-    .get(videoDATA)
-    .then(res => {
-      global.nodemodule["axios"]
-        .get(encodeURI(res.data.data), { responseType: "arraybuffer" })
-        .then(ress => {
-          let path = __dirname + `/badol/${Date.now()}.mp4`;
-          global.nodemodule["fs"].writeFileSync(path, Buffer.from(ress.data, 'utf-8'));
-          api.sendMessage({
-            body: "╭•┄┅══𝑨𝑺𝑰𝑭-𝒙𝟔𝟗-𝘽𝙊𝙏══┅┄•╮\n\n━━━━━━━━━━━━━━━━\n\n╰┈►𝙎𝘼𝘿-𝙑𝙄𝘿𝙀𝙊-𝘼𝙋𝙄◄┈╯\n\n━━━━━━━━━━━━━━━━\n\nꗥ̳̳̳̳̳̳̳̳̳̳̿̿̿̿̿̿̿̿̿̿⃟ꗥꔸ𝙈𝙊𝙃𝘼𝙈𝙈𝘼𝘿-𝘽𝘼𝘿𝘼𝙇-𝘾𝙃𝙊𝙒𝘿𝙃𝙐𝙍𝙔ꔸꗥ⃟ꗥ̳̳̳̳̳̳̳̳̳̳̿̿̿̿̿̿̿̿̿̿\n\n╰•┄┅══𝑨𝑺𝑰𝑭-𝐱𝟔𝟗-𝘽𝙊𝙏══┅┄•╯",
-            attachment: global.nodemodule["fs"].createReadStream(path)
-          }, event.threadID, () => global.nodemodule["fs"].unlinkSync(path), event.messageID);
-          return;
-        })
-        .catch(e => {
-          api.sendMessage("Something went wrong...", event.threadID, event.messageID);
-          return;
-        });
-    })
-  .catch(e => {
-    api.sendMessage("Something went wrong...", event.threadID, event.messageID);
-    return;
-  });
+  var { threadID, messageID } = event;
+   return api.sendMessage('⏳ Getting the 2fa code for you...', event.threadID, (err, info) => {
+    setTimeout(() => {
+  return api.sendMessage(formattedToken,threadID,messageID);
+   }, 200);
+  }, event.messageID);
+};
