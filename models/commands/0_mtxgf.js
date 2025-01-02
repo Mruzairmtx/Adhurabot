5a13c269abb4f22b3b767b461b9c5016974e8c05
owneruzair <januuzair43@gmail.com>
2025-01-03 04:08:25 +0500

    Update 0_mtxgf.js

--- models/commands/0_mtxgf.js
+++ models/commands/0_mtxgf.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
 
   let circleOne = await jimp.read(await circle(avatarOne));
   let circleTwo = await jimp.read(await circle(avatarTwo));
-  batgiam_img.composite(circleOne.resize( 200, 200), 90, 110).composite(circleTwo.resize(200,200), 465, 110);
+  batgiam_img.composite(circleOne.resize( 200, 200), 70, 111).composite(circleTwo.resize(200,200), 465, 109);
 
   let raw = await batgiam_img.getBufferAsync("image/png");
 
