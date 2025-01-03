5f5636e27e5166f671afc59a1752855ec1c8ba2f
Rajputuzair <innoxentuzair9@gmail.com>
2025-01-04 03:06:44 +0500

    Update 0_MTX-UZAIR-TEA.js

--- models/commands/0_MTX-UZAIR-TEA.js
+++ models/commands/0_MTX-UZAIR-TEA.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("Tea")==0 || event.body.indexOf("chai")==0 || event.body.indexOf("Chai")==0 || event.body.indexOf("Chae")==0) {
                 var msg = {
-                                body: "𝐘𝐄 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐂𝐎𝐅𝐅𝐄𝐄 𝐀𝐑𝐀𝐌 𝐂 𝐏𝐈𝐍𝐀 𝐆𝐀𝐑𝐀𝐌 𝐇𝐀𝐈\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝑨𝑫𝑬 𝑩𝒀\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿 ◈ ──── 💚✨",
+                                body: "𝐘𝐄 𝐋𝐎 𝐁𝐀𝐁𝐘 𝐂𝐇𝐀𝐈 𝐀𝐑𝐀𝐌 𝐂 𝐏𝐈𝐍𝐀 𝐆𝐀𝐑𝐀𝐌 𝐇𝐀𝐈\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝑨𝑫𝑬 𝑩𝒀\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿 ◈ ──── 💚✨",
                                 attachment: fs.createReadStream(__dirname + `/uzair/tea.gif`)
                         }
                         api.sendMessage(msg, threadID, messageID);
