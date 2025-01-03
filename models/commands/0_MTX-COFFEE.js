f4b0a2cdd6d3c5793f77a04324a811514186981f
Rajputuzair <innoxentuzair9@gmail.com>
2025-01-04 02:48:39 +0500

    Update 0_MTX-COFFEE.js

--- models/commands/0_MTX-COFFEE.js
+++ models/commands/0_MTX-COFFEE.js
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("Coffee")==0 || event.body.indexOf("coffee")==0 || event.body.indexOf("COFFEE ")==0 || event.body.indexOf("cofi")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐎𝐅𝐅𝐄𝐄 \n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/mtxuzair/Pinterest-boomerang.mp4`)
+                                body: "𝐘𝐞 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐎𝐅𝐅𝐄𝐄 𝐀𝐫𝐚𝐌 𝐒𝐞 𝐏𝐢𝐍𝐚 𝐆𝐚𝐑𝐚𝐌 𝐇𝐚𝐈\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝑨𝑫𝑬 𝑩𝒀\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿 ◈ ──── 💚✨",
+                                attachment: fs.createReadStream(__dirname + `/uzair/coffee.gif`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("☕", event.messageID, (err) => {}, true)
