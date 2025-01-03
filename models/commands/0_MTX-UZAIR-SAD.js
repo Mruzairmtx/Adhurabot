ec6c20e5fbfa1708eca462f3f801e7e294f04c00
Rajputuzair <innoxentuzair9@gmail.com>
2025-01-03 17:51:32 +0500

    Update 0_MTX-UZAIR-SAD.js

--- models/commands/0_MTX-UZAIR-SAD.js
+++ models/commands/0_MTX-UZAIR-SAD.js
@@ -17,7 +17,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = [""];
+   var hi = ["𝑴𝑨𝑫𝑬 𝑩𝒀\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿 ◈ ──── 💚✨"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link =[
 "https://i.imgur.com/9YNDljZ.mp4",
