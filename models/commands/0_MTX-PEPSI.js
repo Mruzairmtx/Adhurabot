e5072df7cea0938b5edb0fa06e3da3f4d5744c6f
uzairmtxbot <uzairrajputmtxbot@gmail.com>
2025-01-06 22:19:04 +0500

    Update 0_MTX-PEPSI.js

--- models/commands/0_MTX-PEPSI.js
+++ models/commands/0_MTX-PEPSI.js
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("pepsi")==0 || event.body.indexOf("Pepsi")==0 || event.body.indexOf("PEPSI")==0 || event.body.indexOf("Coldaring")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕  \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n__________________________________ ",
-                                attachment: fs.createReadStream(__dirname + `/mtxuzair/Pin_di_Shoaib_nazir_su_Quick_Saves(360P).mp4`)
+                                body: "𝐘𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐏𝐄𝐏𝐒𝐈\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝑨𝑫𝑬 𝑩𝒀\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿 ◈ ──── 💚✨",
+                                attachment: fs.createReadStream(__dirname + `/uzair/pepsi.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🥂", event.messageID, (err) => {}, true)
