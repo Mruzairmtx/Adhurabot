bf7817cc2c78b40d2b6289766b0a093a94c92387
uzairmtxbot <uzairrajputmtxbot@gmail.com>
2025-01-06 14:48:06 +0500

    Update 0_MTX-UZAIR-BRTHDY3.js

--- models/commands/0_MTX-UZAIR-BRTHDY3.js
+++ models/commands/0_MTX-UZAIR-BRTHDY3.js
@@ -91,7 +91,7 @@ module.exports.run = async function ({ args, Users, Threads, api, event, Currenc
     ctx.beginPath();
 
 
-  ctx.drawImage(baseAvt1, 170, 170, 170, 371);
+  ctx.drawImage(baseAvt1, 200, 200, 190, 371);
 
   const imageBuffer = canvas.toBuffer();
   fs.writeFileSync(pathImg, imageBuffer);
