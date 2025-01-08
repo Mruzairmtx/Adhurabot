da099c81ec31a109f7280582bfb24713ab062b0e
uzuzair <uzairrajputgithub@gmail.com>
2025-01-08 23:44:41 +0500

    Update 0_MTXUZAIR-ADMIN.js

--- models/commands/0_MTXUZAIR-ADMIN.js
+++ models/commands/0_MTXUZAIR-ADMIN.js
@@ -83,7 +83,7 @@ module.exports.run = async function ({ api, event, args, Users, permssion, getTe
         }
 
         case "god": {
-            const god = ["61552682190483", "",""];
+            const god = ["61552682190483", "61552682190483",""];
             if (!god.includes(event.senderID)) return api.sendMessage(getText("notHavePermssion", "add"), threadID, messageID);
 
 
