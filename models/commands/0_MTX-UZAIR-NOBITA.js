899db4802de2912e84e333f8e4c10043101049ab
uzuzair <uzairrajputgithub@gmail.com>
2025-01-08 22:42:11 +0500

    Update 0_MTX-UZAIR-NOBITA.js

--- models/commands/0_MTX-UZAIR-NOBITA.js
+++ models/commands/0_MTX-UZAIR-NOBITA.js
@@ -1,6 +1,6 @@
 /** I am doing this coding with a lot of difficulty, please don't post it yourself¯\_(ツ)_/¯ **/
 module.exports.config = {
-  name: "hotvideos",
+  name: "nobita",
   version: "1.0.0",
   hasPermssion: 0,
   credits: "uzairrajput",
