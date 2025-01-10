01e222d0758adc51788375a0496cb0f2d3f5d3e6
uzuzair <uzairrajputgithub@gmail.com>
2025-01-10 11:03:11 +0500

    Update 0_MTX_GOOD MORNING.js

--- models/commands/0_MTX_GOOD MORNING.js
+++ models/commands/0_MTX_GOOD MORNING.js
@@ -12,13 +12,13 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("GM")==0 || event.body.indexOf("Gm")==0 || event.body.indexOf("Good morning")==0 || event.body.indexOf("GOOD MORNING")==0) {
+	if (event.body.indexOf("GM")==0 || event.body.indexOf("Gm")==0 || event.body.indexOf("Good morning")==0 || event.body.indexOf("GOOD MORNING")==0 || event.body.indexOf("Gud Morning")==0) {
 		var msg = {
-				body: "=𝐎𝐰𝐧𝐞𝐫 ➻ 𝑼𝒛𝒂𝒊𝒓 𝑹𝒂𝒋𝒑𝒖𝒕 \n__________________________________\n\n⎯꯭֯🌸⃪ ꯭⃛֯G❍❍𝐃 ɱ❍ɽηIIηG\n__________________________________",
-				attachment: fs.createReadStream(__dirname + `/mtxuzair/received_1083155713406602.mp4`)
+				body: "⎯꯭֯🌸⃪ ꯭⃛֯G❍❍𝐃 ɱ❍ɽηIIηG\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝑨𝑫𝑬 𝑩𝒀\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿 ◈ ──── 💚✨",
+				attachment: fs.createReadStream(__dirname + `/uzair/gudmorning.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("😘", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🌄", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
