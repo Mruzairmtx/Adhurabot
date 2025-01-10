5ad2d89aa14761ec91bf38a21904d467af965529
Mtxuzair <uzairaiman1267@gmail.com>
2025-01-10 19:26:52 +0500

    Update 0_MTX_NASTA.js

--- models/commands/0_MTX_NASTA.js
+++ models/commands/0_MTX_NASTA.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "nasta",
+	name: "bhag",
     version: "1.0.1",
 	hasPermssion: 0,
 	credits: "uzairrajput", 
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Ja raha ho")==0 || event.body.indexOf("Jata hu")==0 || event.body.indexOf("Me jao")==0 || event.body.indexOf("jata hu")==0) {
+	if (event.body.indexOf("bhag jaoga")==0 || event.body.indexOf("bhag jaogi")==0 || event.body.indexOf("Bhag Jaoga")==0 || event.body.indexOf("Bhag Jaogi")==0) {
 		var msg = {
 				body: "𝐓𝐔𝐌 𝐀𝐁𝐈 𝐆𝐀𝐘𝐄 𝐍𝐀𝐇𝐈\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝑨𝑫𝑬 𝑩𝒀\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿 ◈ ──── 💚✨ attachment: fs.createReadStream(__dirname + `/uzair/bhag.png`)
 			}
