9d1bbbce00cbeef49a635937184e7db5be7f7a7e
Mtxuzair <uzairaiman1267@gmail.com>
2025-01-10 19:37:36 +0500

    Update 0_MTX_UZAIR-BHAG-JA.js

--- models/commands/0_MTX_UZAIR-BHAG-JA.js
+++ models/commands/0_MTX_UZAIR-BHAG-JA.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("Bhag")==0 || event.body.indexOf("bhag")==0 || event.body.indexOf("Bhag Ja")==0 || event.body.indexOf("bhag Ja")==0) {
 		var msg = {
-				body: "𝐓𝐔𝐌 𝐀𝐁𝐈 𝐆𝐀𝐘𝐄 𝐍𝐀𝐇𝐈\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝑨𝑫𝑬 𝑩𝒀\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿 ◈ ──── 💚✨ attachment: fs.createReadStream(__dirname + `/uzair/bhag.png`)
+				body: "𝐓𝐔 𝐊𝐇𝐔𝐃 𝐉𝐀 🔪😾\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝑨𝑫𝑬 𝑩𝒀\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿 ◈ ──── 💚✨ attachment: fs.createReadStream(__dirname + `/uzair/bhag.png`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("😡", event.messageID, (err) => {}, true)
