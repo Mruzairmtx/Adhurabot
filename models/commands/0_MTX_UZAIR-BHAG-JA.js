bf06b193ea777bd8a0cb27700a669cdd20777a66
Mtxuzair <uzairaiman1267@gmail.com>
2025-01-10 19:33:47 +0500

    Update and rename 0_MTX_NASTA.js to 0_MTX_UZAIR-BHAG-JA.js

--- models/commands/0_MTX_UZAIR-BHAG-JA.js
+++ models/commands/0_MTX_UZAIR-BHAG-JA.js
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("bhag jaoga")==0 || event.body.indexOf("bhag jaogi")==0 || event.body.indexOf("Bhag Jaoga")==0 || event.body.indexOf("Bhag Jaogi")==0) {
+	if (event.body.indexOf("Bhag")==0 || event.body.indexOf("bhag")==0 || event.body.indexOf("Bhag Ja")==0 || event.body.indexOf("bhag Ja")==0) {
 		var msg = {
 				body: "𝐓𝐔𝐌 𝐀𝐁𝐈 𝐆𝐀𝐘𝐄 𝐍𝐀𝐇𝐈\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝑨𝑫𝑬 𝑩𝒀\n\n◈━━━━━━━━━━━━━━━━💚✨\n\n𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿 ◈ ──── 💚✨ attachment: fs.createReadStream(__dirname + `/uzair/bhag.png`)
 			}
