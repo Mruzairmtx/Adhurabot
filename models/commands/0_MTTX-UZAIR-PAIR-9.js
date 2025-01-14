30504286d8dff0eecb2454ce6495c1e5574b82b8
Mtxuzair <uzairaiman1267@gmail.com>
2025-01-14 13:35:02 +0500

    Update 0_MTTX-UZAIR-PAIR-9.js

--- models/commands/0_MTTX-UZAIR-PAIR-9.js
+++ models/commands/0_MTTX-UZAIR-PAIR-9.js
@@ -41,7 +41,7 @@ async function makeImage({ one, two }) {
 
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    pairing_img.composite(circleOne.resize(200, 200), 8, 61).composite(circleTwo.resize(200, 200), 260, 75);
+    pairing_img.composite(circleOne.resize(200, 200), 8, 50).composite(circleTwo.resize(200, 200), 260, 75);
 
     let raw = await pairing_img.getBufferAsync("image/png");
 
