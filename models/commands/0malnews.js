64a81cc7f57c0219d28978db84c5a934014f9125
Mruzairmtx <matlabilogmafatiduniya@gmail.com>
2025-01-14 23:59:25 +0500

    Update 0malnews.js

--- models/commands/0malnews.js
+++ models/commands/0malnews.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "malnews",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "ZiaRein",
+	credits: "uzairrajput",
 	description: "lastest aninews",
 	commandCategory: "anime",
 	usages: " ",
